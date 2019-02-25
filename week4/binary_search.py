45320256fdc823f09e373aa754a44b1542e422b0
Kyle Caverly <kylebcaverly@gmail.com>
2019-02-24 20:07:50 -0500

    2/6 on week4 with progress on binary search

--- week4/binary_search.py
+++ week4/binary_search.py
@@ -1,9 +1,20 @@
 # Uses python3
 import sys
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+def binary_search(a, x, left, right):
+
+    mid = int(left + ((right-left) / 2))
+
+    if left == right and x != a[right]:
+        return -1
+    elif a[left] > x or a[right] < x:
+        return -1
+    elif x == a[mid]:
+        return mid
+    elif x < a[mid]:
+        return binary_search(a, x, 0, mid-1)
+    elif x > a[mid]:
+        return binary_search(a, x, mid+1, len(a) - 1)
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +30,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x, 0, len(a)-1), end = ' ')
--- week4/majority_element.py
+++ week4/majority_element.py
@@ -1,18 +1,31 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
+def get_majority_element(a):
+
+    if len(a) == 0:
         return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
+    if len(a) == 1:
+        return a[0]
+
+    mid = int(len(a) / 2)
+    left = get_majority_element(a[0:mid])
+    right = get_majority_element(a[mid:])
+
+    if left == right:
+        return left
+    elif a.count(left) > mid:
+        return left
+    elif a.count(right) > mid:
+        return right
+
     return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+
+    if get_majority_element(a) != -1:
         print(1)
     else:
         print(0)
